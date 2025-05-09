5667c26d045ebbdcae9a72fbef6dfc968884137b
João Fabris <jvfabris21@gmail.com>
2025-05-09 17:31:41 -0300

    fix: deploy pt4

--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,46 +1,46 @@
-import { db } from "@/app/_lib/prisma";
-import BarberShopInfo from "./_components/barbershop-info";
-import ServiceItem from "./_components/service-item";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+// import { db } from "@/app/_lib/prisma";
+// import BarberShopInfo from "./_components/barbershop-info";
+// import ServiceItem from "./_components/service-item";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
-export default async function BarbershopDetailsPage({
-  params,
-}: {
-  params: { id: string }
-}) {
-  const session = await getServerSession(authOptions);
+// export default async function BarbershopDetailsPage({
+//   params,
+// }: {
+//   params: { id: string }
+// }) {
+//   const session = await getServerSession(authOptions);
 
-  if (!params.id) return null;
+//   if (!params.id) return null;
 
-  const barbershop = await db.barbershop.findUnique({
-    where: { id: params.id },
-    include: { services: true },
-  });
+//   const barbershop = await db.barbershop.findUnique({
+//     where: { id: params.id },
+//     include: { services: true },
+//   });
 
-  if (!barbershop) return null;
+//   if (!barbershop) return null;
 
-  const serializedServices = barbershop.services.map((service) => ({
-    ...service,
-    price: parseFloat(service.price.toString()),
-  }));
+//   const serializedServices = barbershop.services.map((service) => ({
+//     ...service,
+//     price: parseFloat(service.price.toString()),
+//   }));
 
-  const serializedBarbershop = {
-    ...barbershop,
-    services: serializedServices,
-  };
+//   const serializedBarbershop = {
+//     ...barbershop,
+//     services: serializedServices,
+//   };
 
-  return (
-    <div className="px-5 flex flex-col gap-4 py-6">
-      <BarberShopInfo barbershop={serializedBarbershop} />
-      {serializedServices.map((service) => (
-        <ServiceItem
-          key={service.id}
-          barbershop={serializedBarbershop}
-          service={service}
-          isAuthenticated={!!session?.user}
-        />
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div className="px-5 flex flex-col gap-4 py-6">
+//       <BarberShopInfo barbershop={serializedBarbershop} />
+//       {serializedServices.map((service) => (
+//         <ServiceItem
+//           key={service.id}
+//           barbershop={serializedBarbershop}
+//           service={service}
+//           isAuthenticated={!!session?.user}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
